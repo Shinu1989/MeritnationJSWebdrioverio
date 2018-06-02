fbc44d6731504240cc8a83ca02314a278154339e
bansal <bansal.shinu@gmail.com>
2018-06-03 00:41:54 +0530

    my second commit

--- test/testfile.js
+++ test/testfile.js
@@ -2,6 +2,7 @@ var HomePage1 = require('../pages/HomePage');
 var LoginPage = require('../pages/LoginPage.js');
 var commands = require('../core/ActionDriver.js');
 var userPage = require('../pages/UserPage');
+var dataRequired = require('../data/data.json');
 
 describe('login form', function () {
     it('should deny access with wrong creds', function () {
@@ -9,7 +10,8 @@ describe('login form', function () {
         browser.url('/');
         browser.windowHandleMaximize();
        HomePage1.clickLoginLink();
-    LoginPage.loginUserPage("sunaina@test.com","12345678");
+   // LoginPage.loginUserPage("sunaina@test.com","12345678");
+   LoginPage.loginUserPage(dataRequired.username, dataRequired.password);
     userPage.verifyTitle();
 
         
